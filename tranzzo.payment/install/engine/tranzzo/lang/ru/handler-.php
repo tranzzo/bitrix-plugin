3d56a0292601ae7176b54c92d7095dd44278ec79
web13marat <47453587+web13marat@users.noreply.github.com>
2019-10-01 18:04:39 +0300

    Delete handler-.php

--- tranzzo.payment/install/engine/tranzzo/lang/ru/handler-.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS['TRANZZO_CREATE_PAYMENT_ERROR'] = 'При создании формы оплаты возникла ошибка, сервер оплаты вернул сообщение об ошибке!!!';
-$MESS['TRANZZO_BTN_PAY'] = 'Оплатить через TRANZZO';
-$MESS['TRANZZO_SIGNATURE_NOT_VERIFIED'] = 'Ошибка, подпись не подтверждена';
-$MESS['TRANZZO_PAYMENT_FAILED'] = 'Оплата неудалась';
